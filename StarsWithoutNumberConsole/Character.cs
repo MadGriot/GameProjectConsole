74c55b1232dcaae611dfd9faad036bb5a3b335cd
Trenton Scott <InsaneGriot@Outlook.com>
2023-11-13 10:15:02 -0500

    Combat Mechanics 11/13/2023

--- StarsWithoutNumberConsole/Character.cs
+++ StarsWithoutNumberConsole/Character.cs
@@ -1,5 +1,4 @@
-﻿using System.Net.Security;
-
+﻿
 namespace CharacterLibrary
 {
     public class Attributes
@@ -20,37 +19,37 @@ namespace CharacterLibrary
     public class Skills
     {
 
-        public int Administer;
-        public int Connect;
-        public int Exert;
-        public int Fix;
-        public int Heal;
-        public int Know;
-        public int Lead;
-        public int Notice;
-        public int Perform;
-        public int Pilot;
-        public int Program;
-        public int Sneak;
-        public int Survive;
-        public int Persuasion;
-        public int Trade;
-        public int Work;
+        public int Administer = -1;
+        public int Connect = -1;
+        public int Exert = -1;
+        public int Fix = -1;
+        public int Heal = -1;
+        public int Know = -1;
+        public int Lead = -1;
+        public int Notice = -1;
+        public int Perform = -1;
+        public int Pilot = -1;
+        public int Program = -1;
+        public int Sneak = -1;
+        public int Survive = -1;
+        public int Persuasion = -1;
+        public int Trade = -1;
+        public int Work = -1;
         public class Combat
         {
-            public int Martial_Arts;
-            public int Marksmanship;
-            public int Melee_Weapons;
+            public int Martial_Arts = -1;
+            public int Marksmanship = -1;
+            public int Melee_Weapons = -1;
         }
 
         public class Phychic
         {
-            public int Biopsionics;
-            public int Metapsionics;
-            public int Precognition;
-            public int Telekinesis;
-            public int Telepathy;
-            public int Teleportation;
+            public int Biopsionics = -1;
+            public int Metapsionics = -1;
+            public int Precognition = -1;
+            public int Telekinesis = -1;
+            public int Telepathy = -1;
+            public int Teleportation = -1;
         }
     }
     public enum Background
@@ -83,15 +82,44 @@ namespace CharacterLibrary
         Psychic,
         Warrior,
     }
+
+    public class Focus
+    {
+        public int Alert;
+        public int Armsman;
+        public int Assassin;
+        public int Authority;
+        public int Close_Combatant;
+        public int Connected;
+        public int Die_Hard;
+        public int Diplomat;
+        public int Gunslinger;
+        public int Hacker;
+        public int Healer;
+        public int Henchkeeper;
+        public int Ironhide;
+        public int Phychic_Training;
+        public int Savage_Fray;
+        public int Sniper;
+        public int Specialist;
+        public int Star_Captain;
+        public int Starfarer;
+        public int Tinker;
+        public int Unarmed_Combatant;
+        public int Wanderer;
+        public int Wild_Phychic_Talent;
+
+    }
     public class Character
     {
-        public string name;
+        public string name = "";
         public Attributes attributes = new Attributes();
         public Skills character_skills = new Skills();
         public Skills.Combat combat_skills = new Skills.Combat();
         public Skills.Phychic phychic_skills = new Skills.Phychic();
         public Background background;
         public Class character_class;
+        public Focus focus = new Focus();
 
     }
 }
---
+++ StarsWithoutNumberConsole/CombatMechanics.cs
@@ -0,0 +1,15 @@
+﻿using StatMechanicsLib;
+using CharacterLibrary;
+
+namespace CombatMechanicsLib
+{
+    internal class CombatMechanics
+    {
+        StatMechanics mechanics = new StatMechanics();
+        public double initiative(Attributes attributes)
+        {
+            double initiative_roll = mechanics.Modifiers(attributes.Dexterity) + mechanics.D_8();
+            return initiative_roll;
+        }
+    }
+}
